74e0896c83c7722cbf45331122f2ff3eef3b3c20
crlstve <ecarles10@gmail.com>
2024-08-27 22:03:17 +0200

    update templates

--- views/partials/header.php
+++ views/partials/header.php
@@ -7,18 +7,19 @@
     <title>Portafolio Carles Esteve</title>
     <link rel="stylesheet" href="./assets/css/theme.css" type="text/css">
     <script defer src="./assets/js/dark-mode.js"></script>
-    <script defer type="module" src="./assets/js/fireworks.js"></script>
-    <script defer type="module" src="./assets/js/three.js"></script>
+    <!--<script defer type="module" src="./assets/js/fireworks.js"></script>-->
+    <!--<script defer type="module" src="./assets/js/three.js"></script>-->
     <!--<script src="https://cdn.tailwindcss.com"></script>-->
 </head>
-<body id="fireworks" class="min-h-dvh bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-blue-100 via-slate-50  to-gray-50 dark:bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] dark:from-gray-700 dark:via-gray-900 dark:to-black scroll-smooth duration-500">
+<!--<body id="fireworks" class="min-h-dvh bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-blue-100 via-slate-50  to-gray-50 dark:bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] dark:from-gray-700 dark:via-gray-900 dark:to-black scroll-smooth duration-500">-->
+<body id="fireworks" class="min-h-dvh bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-blue-100 via-slate-50  to-gray-50 dark:bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] dark:from-[#282a2d] dark:via-[#101114] dark:to-black scroll-smooth duration-500">
 <header class="sticky top-0 z-50">
     <nav class="w-full py-4 container">
         <div class="flex flex-row justify-between">
             <ul aria="menu" class="flex flex-row items-center gap-3 md:gap-10">
                 <li><a rel="nofollow noreferrer" href="#inicio" class="btn_nav">Inicio</a></li>
                 <li><a rel="nofollow noreferrer" href="#experiencia" class="btn_nav">Experiéncia</a></li>
-               <li><a rel="nofollow noreferrer" href="#proyectos" class="btn_nav">Proyectos</a></li>
+                <li><a rel="nofollow noreferrer" href="#proyectos" class="btn_nav">Proyectos</a></li>
                 <li><a rel="nofollow noreferrer" href="#contacto" class="btn_nav">Contacto</a></li>
             </ul>
             <button class="btn_nav" onclick="toggleDarkMode();">
--- views/templates/front-page.php
+++ views/templates/front-page.php
@@ -1,13 +1,13 @@
 <?php Functions::partial('header'); ?>
    
-<main class="relative w-full min-h-dvh">
-        <div id="bg-dots" class="absolute"></div>
-    <section id="inicio" class="container mx-auto my-6 md:my-12 flex flex-col lg:flex-row">
+<main class="relative w-full min-h-dvh overflow-x-hidden">
+        <div id="bg-dots" class="absolute -z-10"></div>
+    <section id="inicio" class="container mx-auto my-6 md:my-12 flex flex-col-reverse lg:flex-row">
         <article class="w-full lg:w-1/2 flex flex-col gap-3 p-6">
             <header>
-                <h1 class="text-xl font-bold dark:text-[#96da3b] text-violet-500">
-                    Carles Esteve Albiach<br>
-                    Full Stack Web Developer
+                <h1 class="text-xl font-semibold dark:text-white leading-3">
+                    carles esteve albiach<br>
+                    <span class="text-4xl md:text-5xl font-title dark:text-[#b1a36f] text-violet-500">full stack web developer<span>
                 </h1>
             </header>
             <div>
@@ -16,45 +16,88 @@
                 </p>         
             </div>
         </article>
-        <div id="three" class="w-full lg:w-1/2 h-36"></div>
+        <div id="three" class="w-full lg:w-1/2 h-36">
+            <img src="" alt="">
+        </div>
     </section>
-
-
     <section id="experiencia" class="container mx-auto mb-6 lg:mb-12 flex flex-col">
-        <h2 class="p-6 text-xl font-bold dark:text-[#96da3b] text-violet-500">PROYECTOS</h2>
+        <h2 class="p-6 text-xl font-bold dark:text-[#b1a36f] text-violet-500">experiencia</h2>
+        
         <div class="grid grid-cols lg:grid-cols-3 gap-6">
-            <article class="flex flex-col gap-3 p-6 border-l border-t border-white dark:border-slate-600 rounded-md backdrop-blur-sm shadow-md">
-                <header>
-                    <h3 class="text-xl font-bold dark:text-[#96da3b] text-violet-500">Tik Tak Toe</h3>
+            <article class="flex flex-col gap-3 p-6 border-l border-t border-white dark:border-slate-600 rounded-lg backdrop-blur-sm shadow-md dark:bg-[rgba(0,0,0,0.1)]">
+                <header class="">
+                    <h3 class="text-4xl font-title font-bold dark:text-[#b1a36f] text-violet-500">founderz</h3>
+                    <time class="dark:text-white text-sm" datetime="22/04/24">22-04-24 | now</time>
                 </header>
+
                 <div>
                     <p class="text-base dark:text-white font-light">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sapien at libero consequat, id luctus erat posuere. Vestibulum suscipit urna vel felis dapibus, eget interdum metus fermentum.
+                        Desarrollo de templates, temas, plugins y módulos para WordPress y Hubspot. Maquetación de emails. Gestión y mantenimiento de bbdd. Creación de Apis Restful.
                     </p>         
                 </div>
             </article>
 
-            <article class="flex flex-col gap-3 p-6 border-l border-t border-white dark:border-slate-600 rounded-md backdrop-blur-sm shadow-md">
-                <header>
-                    <h3 class="text-xl font-bold dark:text-[#96da3b] text-violet-500">Tetris</h3>
+            <article class="flex flex-col gap-3 p-6 border-l border-t border-white dark:border-slate-600 rounded-lg backdrop-blur-sm shadow-md dark:bg-[rgba(0,0,0,0.1)]">
+                <header class="">
+                    <h3 class="text-4xl font-title font-bold dark:text-[#b1a36f] text-violet-500">sesame hr</h3>
                 </header>
+                <time class="dark:text-white text-sm" datetime="22/04/24">11-06-22 | 22-04-24</time>
                 <div>
                     <p class="text-base dark:text-white font-light">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Phasellus pharetra, elit at luctus tristique, nunc metus interdum sapien, ut dapibus orci lacus at augue.
+                        Desarrollo de templates, temas, plugins y módulos para WordPress y Hubspot. Maquetación de emails. Gestión y mantenimiento de bbdd. Creación de Apis Restful.
                     </p>         
                 </div>
             </article>
 
-            <article class="flex flex-col gap-3 p-6 border-l border-t border-white dark:border-slate-600 rounded-md backdrop-blur-sm shadow-md">
+            <article class="flex flex-col gap-3 p-6 border-l border-t border-white dark:border-slate-600 rounded-lg backdrop-blur-sm shadow-md dark:bg-[rgba(0,0,0,0.1)]">
                 <header>
-                    <h3 class="text-xl font-bold dark:text-[#96da3b] text-violet-500">Zelda</h3>
+                      <h3 class="text-4xl font-title font-bold dark:text-[#b1a36f] text-violet-500">arte regal</h3>
                 </header>
                 <div>
                     <p class="text-base dark:text-white font-light">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id risus magna. Proin egestas, arcu sit amet bibendum facilisis, augue elit sodales quam, sit amet laoreet ligula nulla sit amet metus. Etiam sodales, nisi nec aliquet eleifend.
+                        Desarrollo de la ecommerce de la empresa con Codeigniter. Diseño y maquetación de templates, conexiones a BBDD y desarrollo de Apis Restful. Desarrollo para WordPress, temas, plugins, plantillas y módulos.
                     </p>         
                 </div>
-            </article>            
+            </article> 
+            
+            <article class="flex flex-col gap-3 p-6 border-l border-t border-white dark:border-slate-600 rounded-lg backdrop-blur-sm shadow-md dark:bg-[rgba(0,0,0,0.1)]">
+                <header>
+                      <h3 class="text-4xl font-title font-bold dark:text-[#b1a36f] text-violet-500">availand</h3>
+                </header>
+                <div>
+                    <p class="text-base dark:text-white font-light">
+                        Diseño y desarrollo de las distintas webs de la empresa. Optimización seo de las mismas. Creación de temas, plugins y módulos para WordPress. Maquetación de correos electrónicos y newsletter. Creación de contenido. Grabación de vídeos corporativos, graphic motion y fotografía de producto.
+                    </p>         
+                </div>
+            </article> 
+
+
+            <article class="flex flex-col gap-3 p-6 border-l border-t border-white dark:border-slate-600 rounded-lg backdrop-blur-sm shadow-md dark:bg-[rgba(0,0,0,0.1)]">
+                <header>
+                      <h3 class="text-4xl font-title font-bold dark:text-[#b1a36f] text-violet-500">tv begin</h3>
+                </header>
+                <div>
+                    <p class="text-base dark:text-white font-light">
+                        Diseño de páginas web y apoyo gráfico. Dominio de paquete Adobe (Photoshop, Illustrator, Premiere). Código front-end (HTML5, CSS, Javascript, etc.). Gestión de CMS: Wordpress y Prestashop. Community Manager (Hootsuite) para clientes. Grabación en actos y videos empresariales. Fotografía y retoque fotográfico. Trato directo con clientes.
+                    </p>         
+                </div>
+            </article> 
+
+
+            <article class="flex flex-col gap-3 p-6 border-l border-t border-white dark:border-slate-600 rounded-lg backdrop-blur-sm shadow-md dark:bg-[rgba(0,0,0,0.1)]">
+                <header>
+                      <h3 class="text-4xl font-title font-bold dark:text-[#b1a36f] text-violet-500">galaxa cosmetics</h3>
+                </header>
+                <div>
+                    <p class="text-base dark:text-white font-light">
+                        Técnico en Marketing. Community Manager. Web Developer (diseño, html, css y Wordpress). Especialista SEM/SEO. Diseño
+                        corporativo.
+                    </p>         
+                </div>
+            </article> 
+
+
+
         </div>
     </section>
 
