b09595a9f06e5636163a5b91da81a92b17e6b9e2
crlstve <ecarles10@gmail.com>
2024-09-19 20:53:42 +0200

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,8 +23,9 @@ window.addEventListener('scroll', function () {
         });
     });
 });
-//modal
+//Con el Dom cargado
 document.addEventListener('DOMContentLoaded', function () {
+//modal
     //open modal button
         const modal = document.getElementById('modal');
         const contactButton = document.getElementById('contact');
@@ -34,54 +35,71 @@ document.addEventListener('DOMContentLoaded', function () {
     //close modal button
         modal.addEventListener('click', (e) => { if (e.target === modal) { modal.classList.add('hidden'); } });
     //form
-        const form = document.querySelector('form');
-        const inputs = form.querySelectorAll('input[type="text"], input[type="email"], textarea');
-        const submitButton = form.querySelector('input[type="submit"]');
+    const form = document.querySelector('form');
+    const inputs = form.querySelectorAll('input[type="text"], input[type="email"], textarea');
+    const submitButton = form.querySelector('input[type="submit"]');
     //Validación de campos
-        inputs.forEach(input => {
-            input.addEventListener('blur', function () {
-                if (!this.value.trim()) {
-                    this.classList.add('error');
-                    this.classList.remove('success');
-                } else {
-                    this.classList.add('success');
-                    this.classList.remove('error');
-                }
-            });
-        });
-    // Función para verificar todos los campos
-        function checkAllFields() {
-            return Array.from(inputs).every(input => input.value.trim() !== '');
-        }
-    // Función para actualizar el estado del botón de envío
-        function updateSubmitButton() {
-            if (checkAllFields()) {
-                submitButton.classList.remove('btnError');
-                submitButton.classList.add('btnSuccess');
-                submitButton.disabled = false;
+    inputs.forEach(input => {
+        input.addEventListener('blur', function () {
+            if (!this.value.trim()) {
+                this.classList.add('error');
+                this.classList.remove('success');
             } else {
-                submitButton.classList.remove('btnSuccess');
-                submitButton.classList.add('btnError');
-                submitButton.disabled = true;
+                this.classList.add('success');
+                this.classList.remove('error');
             }
+        });
+    });
+    // Función para verificar todos los campos
+    function checkAllFields() {
+        return Array.from(inputs).every(input => input.value.trim() !== '');
+    }
+    // Función para actualizar el estado del botón de envío
+    function updateSubmitButton() {
+        if (checkAllFields()) {
+            submitButton.classList.remove('btnError');
+            submitButton.classList.add('btnSuccess');
+            submitButton.disabled = false;
+        } else {
+            submitButton.classList.remove('btnSuccess');
+            submitButton.classList.add('btnError');
+            submitButton.disabled = true;
         }
+    }
     // Agregar evento input a cada campo
-        inputs.forEach(input => {
-            input.addEventListener('input', updateSubmitButton);
-        });
+    inputs.forEach(input => {
+        input.addEventListener('input', updateSubmitButton);
+    });
     // Evento submit del formulario
-        form.addEventListener('submit', function(e) {
-            e.preventDefault();
-            if (checkAllFields()) {
-                const result = document.getElementById('success');
-                const form = document.querySelector('form'); // Cambia el selector según sea necesario
-                form.classList.add('hidden');
-                result.classList.remove('hidden');
-                form.reset();
-            }
-        });
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        if (checkAllFields()) {
+            const result = document.getElementById('success');
+            const form = document.querySelector('form'); // Cambia el selector según sea necesario
+            form.classList.add('hidden');
+            result.classList.remove('hidden');
+
+            // Recoger los datos del formulario y crear un objeto
+            var data = {
+                nombre: form.querySelector('[name="name"]').value,
+                email: form.querySelector('[name="email"]').value,
+                subject: form.querySelector('[name="subject"]').value,
+                message: form.querySelector('[name="message"]').value
+            };
+            
+            // Crear un objeto XMLHttpRequest
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', './controllers/contact.php', true);
+            console.log(data);
+            // Configurar el encabezado para indicar que el cuerpo de la petición es JSON
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            // Enviar los datos como JSON
+            xhr.send(JSON.stringify(data));
+
+        }
+    });
     // Actualizar el estado inicial del botón
-        updateSubmitButton();
+    updateSubmitButton();
     //mensaje
     console.log(
         '¡Hola! Me llamo %cCarles Esteve%c, soy %cdesarrollador web full stack%c y soy de %cValencia, España%c.\n' +
