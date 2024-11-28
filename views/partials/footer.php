2e7274191007a9494cff39f6ec1e0356b5f33daf
crlstve <ecarles10@gmail.com>
2024-11-28 15:27:13 +0100

    updates

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="container py-6 flex justify-end">
-        <p class="dark:text-white text-xs font-bold"><?= 'c.esteve  ' . date("Y"); ?></p>
+        <span class="dark:text-white text-xs font-bold"><?= 'c.esteve  ' . date("Y"); ?></span>
     </div>
 </footer>
 </body>
