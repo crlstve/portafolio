1676d2763550625ffbe24c68cea8628afb8531e5
crlstve <ecarles10@gmail.com>
2025-11-20 21:22:56 +0100

    update structure

--- views/partials/experience.php
+++ views/partials/experience.php
@@ -1,25 +1,34 @@
     <section id="experiencia" class="max-w-6xl mx-auto mb-6 lg:py-12 flex flex-col">
+
         <h2 data-text="experiencia" class="w-fit mx-auto font-title p-6 text-3xl md:text-4xl text-center font-bold leading-6 mb-6">experiencia</h2>
-        <div class="grid grid-cols-1 md:grid-cols-12 snap-y snap-mandatory ">
-            <ul class="md:col-span-5 flex flex-col gap-5">
-                <li class="snap-always card fadeInUp visible">
-                    <header>
-                        <h3 class="text-4xl font-title font-bold leading-6 mb-1">Cámara Valencia</h3>
-                        <time class="text-sm" datetime="01/04/25">01-04-25 | act</time>
-                        <a rel="nofollow noopener noreferrer" target="_blank" aria-label="Visita la web de Cámara Valencia" href="https://www.camaravalencia.com/" class="link">
-                            <?= Functions::svg(); ?>
-                        </a>
-                    </header>
-                    <article>
+
+        <div class="grid grid-cols-1 md:grid-cols-13 snap-y snap-mandatory ">
+
+            <ul class="md:col-span-6 flex flex-col gap-24 md:my-12">
+
+                <li class="snap-always fadeInUp visible relative grid grid-cols-12">
+                    <article class="card col-span-11">
+                        <header>
+                            <h3 class="text-4xl font-title font-bold leading-6 mb-1">cámara valencia</h3>
+                            
+                            <a rel="nofollow noopener noreferrer" target="_blank" aria-label="Visita la web de Cámara Valencia" href="https://www.camaravalencia.com/" class="link">
+                                <?= Functions::svg(); ?>
+                            </a>
+                        </header>
+                    
                         <p class="text-base">
                             Actualización y mantenimiento de la web corporativa de la empresa. Desarrollo de nuevas funcionalidades y mejoras en la experiencia de usuario. Programación con <b>Wordpress</b> y <b>Laravel</b>. Desarrollo de <b>APIS REST</b>.
                         </p>
                     </article>
+                    <span class="w-full top-0 right-0 col-span-1 text-sm writing-rl justify-center items-center flex">
+                        01-10-24 | actual
+                    </span>
                 </li>
+
                 <li class="card snap-always fadeInUp">
                     <header>
                         <h3 class="text-4xl font-title font-bold leading-6 mb-1">founderz</h3>
-                        <time class="text-sm" datetime="22/04/24">22-04-24 | 30-09-24</time>
+                        <span class="text-sm" datetime="22/04/24">22-04-24 | 30-09-24</span>
                         <a rel="nofollow noopener noreferrer" target="_blank" aria-label="Visita la web de Founderz" href="https://founderz.com/" class="link">
                             <?= Functions::svg(); ?>
                         </a>
@@ -30,10 +39,11 @@
                         </p>
                     </article>
                 </li>
+
                 <li class="card fadeInUp">
                     <header>
                         <h3 class="text-4xl font-title font-bold leading-6 mb-1">arte regal</h3>
-                        <time class="text-sm" datetime="04/10/20">04-10-2020 | 10-06-2022</time>
+                        <span class="text-sm" datetime="04/10/20">04-10-2020 | 10-06-2022</span>
                         <a rel="nofollow noopener noreferrer" target="_blank" aria-label="Visita la web de Arte Regal Import" href="https://www.arteregal.com/" class="link">
                             <?= Functions::svg(); ?>
                         </a>
@@ -44,10 +54,11 @@
                         </p>
                     </article>
                 </li>
+
                 <li class="card fadeInUp">
                     <header>
                         <h3 class="text-4xl font-title font-bold leading-6 mb-1">tv begin</h3>
-                        <time class="text-sm" datetime="22/04/24">04-09-2017 | 04-05-2019</time>
+                        <span class="text-sm" datetime="22/04/24">04-09-2017 | 04-05-2019</span>
                         <a rel="nofollow noopener noreferrer" target="" aria-label="Visita la web de Tv Begin" href="https://tvbgn.com/" class="link">
                             <?= Functions::svg(); ?>
                         </a>
@@ -58,22 +69,29 @@
                         </p>
                     </article>
                 </li>
+
             </ul>
 
-            <div class="md:col-span-2 h-full border-x border-x-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:var(--color-black)]/5 md:block mx-3">
+            <div class="md:col-span-1 h-full border-x border-x-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:var(--color-black)]/5 md:block dark:[--pattern-fg:var(--color-white)]/10 w-8/12  mx-auto">
+                <ul class="w-full realtive mt-12 fle flex-col">
+                </ul>
             </div>
 
-            <ul class="md:col-span-5 md:col-start-8 flex flex-col gap-5 md:mt-10">
+            <ul class="md:col-span-6 md:col-start-8 flex flex-col gap-24 md:mt-32 md:mb-12">
             
-                <li class="card fadeInUp visible ">
+                <li class="fadeInUp visible grid-cols-12 relative grid snap-always">
+                    <span class="w-full top-0 right-0 col-span-1 text-sm writing-lr justify-center items-center flex">01-10-24 | 01-04-25</span>
+                <article class="card col-span-11">
+
+  
                     <header>
-                        <h3 class="text-4xl font-title font-bold leading-6 mb-1">Idital</h3>
-                        <time class="text-sm" datetime="11/06/22">01-10-24 | 01-04-25</time>
+                        <h3 class="text-4xl font-title font-bold leading-6 mb-1">idital</h3>
+                       
                         <a rel="nofollow noopener noreferrer" target="_blank" aria-label="Visita la web de Idital" href="https://www.idital.com/" class="link">
                             <?= Functions::svg(); ?>
                         </a>
                     </header>
-                    <article>
+
                         <p class="text-base">
                             Desarrollo de webs y aplicaciones a medida para clientes de la empresa. Programación con <b>Laravel</b>, <b>Wordpress</b> y <b>Vue.js</b>. Desarrollo de <b>APIS REST</b> y <b>APIS GraphQL</b>. Integración con <b>APIS de terceros</b>.
                         </p>
@@ -81,14 +99,14 @@
                 </li>
 
                 <li class="card fadeInUp">
-                    <header>
-                        <h3 class="text-4xl font-title font-bold leading-6 mb-1">sesame hr</h3>
-                        <time class="text-sm" datetime="11/06/22">11-06-2022 | 22-04-2024</time>
-                        <a rel="nofollow noopener noreferrer" target="_blank" aria-label="Visita la web de Sesame HR" href="https://www.sesamehr.es/" class="link">
-                            <?= Functions::svg(); ?>
-                        </a>
-                    </header>
                     <article>
+                        <header>
+                            <h3 class="text-4xl font-title font-bold leading-6 mb-1">sesame hr</h3>
+                            <span class="text-sm" datetime="11/06/22">11-06-2022 | 22-04-2024</span>
+                            <a rel="nofollow noopener noreferrer" target="_blank" aria-label="Visita la web de Sesame HR" href="https://www.sesamehr.es/" class="link">
+                                <?= Functions::svg(); ?>
+                            </a>
+                        </header>
                         <p class="text-base">
                             Creación y mantenimiento de las distintas webs de la empresa. Desarrollo de la actualización y optimización de la web a nivel SEO y CRO de estas. Programación de <b>plugins</b>, <b>temas</b>, <b>bloques</b> y <b>APIS</b> para Wordpress. Desarrollo de landing pages para Hubspot con <b>Hubl</b>.
                         </p>
@@ -98,7 +116,7 @@
                 <li class="card snap-always fadeInUp">
                     <header>
                         <h3 class="text-4xl font-title font-bold leading-6 mb-1">availand</h3>
-                        <time class="text-sm" datetime="22/04/24">04-05-2019 | 03-06-2020</time>
+                        <span class="text-sm" datetime="22/04/24">04-05-2019 | 03-06-2020</span>
                         <a rel="nofollow noopener noreferrer" target="_blank" aria-label="Visita la web de Availand" href="https://availand.com/" class="link">
                             <?= Functions::svg(); ?>
                         </a>
@@ -113,7 +131,7 @@
                 <li class="card snap-always fadeInUp">
                     <header>
                         <h3 class="text-4xl font-title font-bold leading-6 mb-1">galaxa cosmetics</h3>
-                        <time class="text-sm" datetime="22/04/24">13-04-2016 | 10-06-2017</time>
+                        <span class="text-sm" datetime="22/04/24">13-04-2016 | 10-06-2017</span>
                         <a rel="nofollow noopener noreferrer" target="" aria-label="Visita la web de Galaxa Cosmetics" href="https://productos-peluqueria-online.es/" class="link"> <?= Functions::svg(); ?>
                         </a>
                     </header>
@@ -125,5 +143,7 @@
                 </li>
 
             </ul>
+
         </div>
+
     </section>
