bc9f1969c7a264eaca2b49734d59c5a2336abaf7
crlstve <ecarles10@gmail.com>
2025-12-18 12:46:00 +0100

    rounded

--- views/partials/header.php
+++ views/partials/header.php
@@ -35,32 +35,32 @@
     <script defer type="module" src="./assets/js/splide-extension-auto-scroll.min.js"></script>
     <script src="https://www.google.com/recaptcha/enterprise.js?render=6LchZOYrAAAAANd45lDNB77QKVI5WaMgcd0HvS0o"></script>
     <script>
-  function onClick(e) {
-    e.preventDefault();
-    grecaptcha.enterprise.ready(async () => {
-      const token = await grecaptcha.enterprise.execute('6LchZOYrAAAAANd45lDNB77QKVI5WaMgcd0HvS0o', {action: 'SUBMIT'});
-    });
-  }
-</script>
+        function onClick(e) {
+            e.preventDefault();
+            grecaptcha.enterprise.ready(async () => {
+            const token = await grecaptcha.enterprise.execute('6LchZOYrAAAAANd45lDNB77QKVI5WaMgcd0HvS0o', {action: 'SUBMIT'});
+            });
+        }
+    </script>
 </head>
 <body id="fireworks" class="min-h-dvh bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-slate-50 via-slate-100  to-gray-50 dark:bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] dark:from-[#282a2d] dark:via-[#101114] dark:to-black scroll-smooth duration-500 crt">
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NQ3W6XTC"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->    
-    <header class="sticky top-0 z-10 crt">
+    <header class="sticky top-0 z-10 crt rounded-full">
         <div class="w-full py-4 container">
-            <nav class="flex flex-row justify-end md:justify-center relative">
+            <nav class="flex flex-row justify-end md:justify-center relative rounded-full">
                     <svg data-contoller="menu" class="z-10 cursor-pointer md:hidden w-10 h-10 aspect-square  sound rounded-lg p-2 backdrop-blur-sm border-l border-t border-white dark:border-slate-600 dark:bg-[rgba(0,0,0,0.1)] bg-[rgba(255,255,255,0.1)] flex justify-center" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="23" height="23" viewBox="0,0,260,260">
                     <g class="dark:fill-white fill-slate-600" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(5.12,5.12)"><path d="M3,9c-0.36064,-0.0051 -0.69608,0.18438 -0.87789,0.49587c-0.18181,0.3115 -0.18181,0.69676 0,1.00825c0.18181,0.3115 0.51725,0.50097 0.87789,0.49587h44c0.36064,0.0051 0.69608,-0.18438 0.87789,-0.49587c0.18181,-0.3115 0.18181,-0.69676 0,-1.00825c-0.18181,-0.3115 -0.51725,-0.50097 -0.87789,-0.49587zM3,24c-0.36064,-0.0051 -0.69608,0.18438 -0.87789,0.49587c-0.18181,0.3115 -0.18181,0.69676 0,1.00825c0.18181,0.3115 0.51725,0.50097 0.87789,0.49587h44c0.36064,0.0051 0.69608,-0.18438 0.87789,-0.49587c0.18181,-0.3115 0.18181,-0.69676 0,-1.00825c-0.18181,-0.3115 -0.51725,-0.50097 -0.87789,-0.49587zM3,39c-0.36064,-0.0051 -0.69608,0.18438 -0.87789,0.49587c-0.18181,0.3115 -0.18181,0.69676 0,1.00825c0.18181,0.3115 0.51725,0.50097 0.87789,0.49587h44c0.36064,0.0051 0.69608,-0.18438 0.87789,-0.49587c0.18181,-0.3115 0.18181,-0.69676 0,-1.00825c-0.18181,-0.3115 -0.51725,-0.50097 -0.87789,-0.49587z"></path></g></g>
                     </svg>
-                <ul id="nav-menu" aria="menu" class="fixed w-screen md:w-fit md:relative hidden md:flex md:flex-row flex-wrap justify-center items-center gap-3 md:gap-10 px-6 py-16 md:py-3  backdrop-blur-sm border-l border-t border-white dark:border-slate-600 dark:bg-[rgba(0,0,0,0.1)] bg-[rgba(255,255,255,0.1)] duration-500 top-0 left-0 right-0 bottom-0">
+                <ul id="nav-menu" aria="menu" class="fixed w-screen md:w-fit md:relative hidden md:flex md:flex-row flex-wrap justify-center items-center gap-3 md:gap-10 px-6 py-16 md:py-3  backdrop-blur-sm border-l border-t border-white dark:border-slate-600 dark:bg-[rgba(0,0,0,0.1)] bg-[rgba(255,255,255,0.1)] duration-500 top-0 left-0 right-0 bottom-0 rounded-full">
                     <li><a aria="menu item" rel="nofollow noreferrer" href="#experiencia" class="btn_nav sound">experiencia</a></li>
                     <li><a aria="menu item" rel="nofollow noreferrer" href="#proyectos" class="btn_nav sound">proyectos</a></li>
                     <li><a aria="menu item" rel="nofollow noreferrer" href="#stack" class="btn_nav sound">stack</a></li>
                     <li><button id="contact" aria="menu item" aria-label="Contacto" class="btn_nav sound w-full md:w-fit">contacto</button></a>
                     <li class="flex justify-end md:justify-center">
-                        <button id="btn_mode" aria="menu item" aria-label="Modo oscuro" class="sound group p-2 aspect-square bg-transparent relative border-l border-t border-white hover:border-slate-300 hover:dark:border-slate-950 dark:border-slate-600 backdrop-blur hover:backdrop-blur-none overflow-hidden cursor-pointer" onclick="toggleDarkMode();">
+                        <button id="btn_mode" aria="menu item" aria-label="Modo oscuro" class="sound group p-2 aspect-square bg-transparent relative border-l border-t border-white hover:border-slate-300 hover:dark:border-slate-950 dark:border-slate-600 backdrop-blur hover:backdrop-blur-none overflow-hidden cursor-pointer rounded-full" onclick="toggleDarkMode();">
                             <svg class="fill-slate-600 group-hover:fill-violet-500 dark:fill-white group-hover:dark:fill-emerald-300 dark:translate-y-0 group-hover:dark:-translate-y-8 duration-500 -translate-y-8 group-hover:-translate-y-0 group-hover:dark" version="1.1" id="moon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20px" height="20px" viewBox="0 0 30.457 30.457" xml:space="preserve">
                             <g><path d="M29.693,14.49c-0.469-0.174-1-0.035-1.32,0.353c-1.795,2.189-4.443,3.446-7.27,3.446c-5.183,0-9.396-4.216-9.396-9.397c0-2.608,1.051-5.036,2.963-6.835c0.366-0.347,0.471-0.885,0.264-1.343c-0.207-0.456-0.682-0.736-1.184-0.684 C5.91,0.791,0,7.311,0,15.194c0,8.402,6.836,15.238,15.238,15.238c8.303,0,14.989-6.506,15.219-14.812 C30.471,15.118,30.164,14.664,29.693,14.49z" /></g>
                             </svg>
