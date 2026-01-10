6e635acf8269f75e37713b37f3d0157e4410af05
crlstve <ecarles10@gmail.com>
2026-01-10 20:15:09 +0100

    doble columna exp/stack

--- views/templates/front-page.php
+++ views/templates/front-page.php
@@ -6,10 +6,13 @@
         
         Functions::partial('experience'); 
 
-        Functions::partial('projects'); 
+    ?>
+    <section class="max-w-6xl mx-auto mb-6 lg:py-12 grid grid-cols-12 gap-6 px-6 lg:px-0 lg:gap-12 lg:grid-flow-dense">
+        <?php Functions::partial('projects');  ?>
 
-        Functions::partial('stack'); 
-
+        <?php Functions::partial('stack');?>
+    </section>
+    <?php
         Functions::partial('modal'); 
     ?>
 
