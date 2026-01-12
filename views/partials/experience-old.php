e59f60d75c84fe3b7034392d1c582d00b208f853
crlstve <ecarles10@gmail.com>
2026-01-12 21:27:40 +0100

    vieja versión

---
+++ views/partials/experience-old.php
@@ -0,0 +1,127 @@
+    <section id="experiencia" class="container mx-auto mb-6 lg:py-12 flex flex-col">
+        <h2 data-text="experiencia" class="w-fit mx-auto font-title p-6 text-3xl md:text-4xl text-center font-bold dark:text-emerald-300 text-violet-500 leading-4 mb-6">experiencia</h2>
+        <div class="flex snap-y snap-mandatory">
+            <ul class="timeline relative w-full flex flex-wrap gap-y-6 md:gap-y-12 justify-between">
+            
+                <li class="bullet card after:top-0 before:top-0 snap-always md:mt-6 fadeInUp visible">
+                    <header>
+                        <h3 class="text-4xl font-title font-bold dark:text-emerald-300 text-violet-500 leading-4 mb-1">Cámara Valencia</h3>
+                        <time class="text-sm" datetime="01/04/25">01-04-25 | act</time>
+                        <a rel="nofollow noopener noreferrer" target="_blank" aria-label="Visita la web de Cámara Valencia" href="https://www.camaravalencia.com/" class="link">
+                            <?= Functions::svg(); ?>
+                        </a>
+                    </header>
+                    <article>
+                        <p class="text-base">
+                            Actualización y mantenimiento de la web corporativa de la empresa. Desarrollo de nuevas funcionalidades y mejoras en la experiencia de usuario. Programación con <b>Wordpress</b> y <b>Laravel</b>. Desarrollo de <b>APIS REST</b>.
+                        </p>
+                    </article>
+                </li>
+
+                <li class="bullet card after:top-20 before:top-20 md:mt-24 fadeInUp visible">
+                    <header>
+                        <h3 class="text-4xl font-title font-bold dark:text-emerald-300 text-violet-500 leading-4 mb-1">Idital</h3>
+                        <time class="text-sm" datetime="11/06/22">01-10-24 | 01-04-25</time>
+                        <a rel="nofollow noopener noreferrer" target="_blank" aria-label="Visita la web de Idital" href="https://www.idital.com/" class="link">
+                            <?= Functions::svg(); ?>
+                        </a>
+                    </header>
+                    <article>
+                        <p class="text-base">
+                            Desarrollo de webs y aplicaciones a medida para clientes de la empresa. Programación con <b>Laravel</b>, <b>Wordpress</b> y <b>Vue.js</b>. Desarrollo de <b>APIS REST</b> y <b>APIS GraphQL</b>. Integración con <b>APIS de terceros</b>.
+                        </p>
+                    </article>
+                </li>
+
+                <li class="bullet card after:top-10 before:top-10 snap-always md:-mt-24 fadeInUp">
+                    <header>
+                        <h3 class="text-4xl font-title font-bold dark:text-emerald-300 text-violet-500 leading-4 mb-1">founderz</h3>
+                        <time class="text-sm" datetime="22/04/24">22-04-24 | 30-09-24</time>
+                        <a rel="nofollow noopener noreferrer" target="_blank" aria-label="Visita la web de Founderz" href="https://founderz.com/" class="link">
+                            <?= Functions::svg(); ?>
+                        </a>
+                    </header>
+                    <article>
+                        <p class="text-base">
+                            Desarrollo web con <b>Wordpress</b> y <b>Laravel</b>. Liderar la modernización y rebranding completo de la web, programación de <b>bloques de Gutenberg</b>, <b>templates</b> y <b>plugins</b> para Wordpress. Desarrollo de <b>APIS REST</b>.
+                        </p>
+                    </article>
+                </li>
+
+                <li class="bullet card after:top-20 before:top-20 fadeInUp">
+                    <header>
+                        <h3 class="text-4xl font-title font-bold dark:text-emerald-300 text-violet-500 leading-4 mb-1">sesame hr</h3>
+                        <time class="text-sm" datetime="11/06/22">11-06-2022 | 22-04-2024</time>
+                        <a rel="nofollow noopener noreferrer" target="_blank" aria-label="Visita la web de Sesame HR" href="https://www.sesamehr.es/" class="link">
+                            <?= Functions::svg(); ?>
+                        </a>
+                    </header>
+                    <article>
+                        <p class="text-base">
+                            Creación y mantenimiento de las distintas webs de la empresa. Desarrollo de la actualización y optimización de la web a nivel SEO y CRO de estas. Programación de <b>plugins</b>, <b>temas</b>, <b>bloques</b> y <b>APIS</b> para Wordpress. Desarrollo de landing pages para Hubspot con <b>Hubl</b>.
+                        </p>
+                    </article>
+                </li>
+
+                <li class="bullet card after:top-0 before:top-0 md:-mt-24 fadeInUp">
+                    <header>
+                        <h3 class="text-4xl font-title font-bold dark:text-emerald-300 text-violet-500 leading-4 mb-1">arte regal</h3>
+                        <time class="text-sm" datetime="04/10/20">04-10-2020 | 10-06-2022</time>
+                        <a rel="nofollow noopener noreferrer" target="_blank" aria-label="Visita la web de Arte Regal Import" href="https://www.arteregal.com/" class="link">
+                            <?= Functions::svg(); ?>
+                        </a>
+                    </header>
+                    <article>
+                        <p class="text-base">
+                            Mantenimiento y actualización de la ecommerce de la empresa hecha con <b>Codeigniter</b>. Desarrollo de otras webs y ecommerce hechas con <b>Wordpress</b>. Desarrollo de <b>APIS</b>, <b>bloques</b>, <b>temas</b> y <b>plugins</b> para <b>Wordpress</b>.
+                        </p>
+                    </article>
+                </li>
+
+                <li class="bullet card after:top-10 before:top-10 snap-always fadeInUp">
+                    <header>
+                        <h3 class="text-4xl font-title font-bold dark:text-emerald-300 text-violet-500 leading-4 mb-1">availand</h3>
+                        <time class="text-sm" datetime="22/04/24">04-05-2019 | 03-06-2020</time>
+                        <a rel="nofollow noopener noreferrer" target="_blank" aria-label="Visita la web de Availand" href="https://availand.com/" class="link">
+                            <?= Functions::svg(); ?>
+                        </a>
+                    </header>
+                    <article>
+                        <p class="text-base">
+                            Mantenimiento y actualización de la ecommerce de la empresa hecha con <b>wordpress</b> y <b>woocommerce</b>. Desarrollo de <b>APIS</b>, <b>bloques</b>, <b>temas</b> de <b>Gutenberg</b> y <b>plugins</b>.
+                        </p>
+                    </article>
+                </li>
+
+                <li class="bullet card after:top-0 before:top-0 md:-mt-16 fadeInUp">
+                    <header>
+                        <h3 class="text-4xl font-title font-bold dark:text-emerald-300 text-violet-500 leading-4 mb-1">tv begin</h3>
+                        <time class="text-sm" datetime="22/04/24">04-09-2017 | 04-05-2019</time>
+                        <a rel="nofollow noopener noreferrer" target="" aria-label="Visita la web de Tv Begin" href="https://tvbgn.com/" class="link">
+                            <?= Functions::svg(); ?>
+                        </a>
+                    </header>
+                    <article>
+                        <p class="text-base">
+                            Creación y mantenmiento de webs para distintos clientes hechas con Wordpress. Creación de <b>APIS</b>, <b>bloques</b>, <b>plugins</b>, <b>temas</b> y <b>módulos</b> para resolver las necesidades de las emrpesas.
+                        </p>
+                    </article>
+                </li>
+
+                <li class="bullet card after:top-10 before:top-10 snap-always fadeInUp">
+                    <header>
+                        <h3 class="text-4xl font-title font-bold dark:text-emerald-300 text-violet-500 leading-4 mb-1">galaxa cosmetics</h3>
+                        <time class="text-sm" datetime="22/04/24">13-04-2016 | 10-06-2017</time>
+                        <a rel="nofollow noopener noreferrer" target="" aria-label="Visita la web de Galaxa Cosmetics" href="https://productos-peluqueria-online.es/" class="link"> <?= Functions::svg(); ?>
+                        </a>
+                    </header>
+                    <article>
+                        <p class="text-base">
+                            Desarrollo de la ecommerce de la empresa hecha con <b>wordpress</b> y <b>woocommerce</b>. Desarollo de <b>plugins</b>, <b>APIS</b>, <b>temas</b> y <b>módulos</b> para las necesidades de la empresa
+                        </p>
+                    </article>
+                </li>
+
+            </ul>
+        </div>
+    </section>
