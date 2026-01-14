eb0bedc1a486d17b32c30dfed13451631540b215
crlstve <ecarles10@gmail.com>
2026-01-14 19:51:11 +0100

    update responsive style

--- views/partials/experience.php
+++ views/partials/experience.php
@@ -1,11 +1,11 @@
-    <section id="experiencia" class="max-w-6xl mx-auto mb-6 lg:py-12 flex flex-col relative">
+    <section id="experiencia" class="max-w-6xl mx-auto mb-6 xl:py-12 flex flex-col relative">
 
-        <h2 data-text="experiencia" class="w-fit mx-auto font-title p-6 text-3xl md:text-2xl text-center font-bold leading-6 mb-6">experiencia</h2>
+        <h2 data-text="experiencia" class="w-fit mx-auto font-title p-6 text-3xl xl:text-2xl text-center font-bold leading-6 mb-6">experiencia</h2>
 
         <div class=" snap-y snap-mandatory ">
-            <ul class="timeline grid grid-cols-1 md:grid-cols-12 gap-24 md:my-12 bg-transparent relative">
+            <ul class="timeline grid grid-cols-1 xl:grid-cols-12 gap-24 xl:my-12 bg-transparent relative">
                 <!-- Cámara Valencia -->
-                <li class="snap-always md:col-span-6 fadeInUp visible relative grid grid-cols-12 bg-transparent">
+                <li class="snap-always xl:col-span-6 fadeInUp visible relative grid grid-cols-12 bg-transparent">
                     <article class="card col-span-9 z-10 backdrop-blur-sm relative">
                         <header>
                             <h3 class="text-2xl font-title font-bold leading-6 mb-1">cámara valencia</h3>
@@ -17,16 +17,16 @@
                             Actualización y mantenimiento de la web corporativa de la empresa. Desarrollo de nuevas funcionalidades y mejoras en la experiencia de usuario. Programación con <b>Wordpress</b> y <b>Laravel</b>. Desarrollo de <b>APIS REST</b>.
                         </p>
                     </article>
-                    <span class="border-slate-200 dark:border-dark-border border-t-1 w-full h-1 col-span-3  mt-4">
+                    <span class="border-slate-200 dark:border-dark-border border-t-1 w-full h-1 col-span-1 hidden xl:block xl:col-span-3  mt-4">
                     </span>
-                    <span class="w-fit h-fit py-2 px-6 top-0 col-span-3 text-sm text-center flex justify-center items-center border border-slate-200 dark:border-dark-border absolute right-0 -mr-25 backdrop-blur-sm">
+                    <span class="w-fit h-fit py-2 px-6 top-0 col-span-3 text-sm text-center flex justify-center items-center border border-slate-200 dark:border-dark-border absolute right-0 xl:-mr-25 backdrop-blur-sm">
                         actual<br>04/25
                     </span> 
                 </li>
                 <!-- Idital -->
-                <li class="fadeInUp visible relative grid grid-cols-12 snap-always md:col-span-6 md:mt-28">                  
-                    <span class="w-fit h-fit py-2 px-6 top-0 col-span-3 text-sm text-center flex justify-center items-center border border-slate-200 dark:border-dark-border absolute left-0 -ml-23 backdrop-blur-sm">04/25<br>10/24</span>
-                    <span class="border-slate-200 dark:border-dark-border border-t-1 w-full h-1 col-span-3 mt-4">
+                <li class="fadeInUp visible relative grid grid-cols-12 snap-always xl:col-span-6 xl:mt-28">                  
+                    <span class="w-fit h-fit py-2 px-6 top-0 col-span-3 text-sm text-center flex justify-center items-center border border-slate-200 dark:border-dark-border absolute left-0 xl:-ml-23 backdrop-blur-sm">04/25<br>10/24</span>
+                    <span class="border-slate-200 dark:border-dark-border border-t-1 w-full h-1 col-span-1 hidden xl:block xl:col-span-3 mt-4">
                     </span> 
                     <article class="card col-span-9 col-start-4 z-10 backdrop-blur-sm">
                         <header>
@@ -41,7 +41,7 @@
                     </article>
                 </li>
                 <!-- Founderz -->
-                <li class="snap-always md:col-span-6 fadeInUp relative grid grid-cols-12 md:-mt-53">
+                <li class="snap-always xl:col-span-6 fadeInUp relative grid grid-cols-12 xl:-mt-53">
 
                     <article class="card col-span-9 z-10 backdrop-blur-sm">
                         <header>
@@ -54,13 +54,13 @@
                             Desarrollo web con <b>Wordpress</b> y <b>Laravel</b>. Liderar la modernización y rebranding completo de la web, programación de <b>bloques de Gutenberg</b>, <b>templates</b> y <b>plugins</b> para Wordpress. Desarrollo de <b>APIS REST</b>.
                         </p>
                     </article>
-                    <span class="border-slate-200 dark:border-dark-border border-t-1 w-full h-1 col-span-3 mt-4 ">
+                    <span class="border-slate-200 dark:border-dark-border border-t-1 w-full h-1 col-span-1 hidden xl:block xl:col-span-3 mt-4 ">
                     </span>
-                    <span class="w-fit h-fit py-2 px-6 top-0 col-span-3 text-sm text-center flex justify-center items-center border border-slate-200 dark:border-dark-border absolute right-0 -mr-23 backdrop-blur-sm">09/24<br>04/24</span>
+                    <span class="w-fit h-fit py-2 px-6 top-0 col-span-3 text-sm text-center flex justify-center items-center border border-slate-200 dark:border-dark-border absolute right-0 xl:-mr-23 backdrop-blur-sm">09/24<br>04/24</span>
                 </li>
                 <!-- Sesame HR -->
-                <li class="fadeInUp relative grid grid-cols-12 snap-always md:col-span-6 md:-mt-20">
-                    <span class="w-fit h-fit py-2 px-6 top-0 col-span-3 text-sm text-center flex justify-center items-center border border-slate-200 dark:border-dark-border absolute left-0 -ml-23 backdrop-blur-sm">04/24<br>06/22</span>                     <span class="border-slate-200 dark:border-dark-border border-t-1 w-full h-1 col-span-3 mt-4">
+                <li class="fadeInUp relative grid grid-cols-12 snap-always xl:col-span-6 xl:-mt-20">
+                    <span class="w-fit h-fit py-2 px-6 top-0 col-span-3 text-sm text-center flex justify-center items-center border border-slate-200 dark:border-dark-border absolute left-0 xl:-ml-23 backdrop-blur-sm">04/24<br>06/22</span>                     <span class="border-slate-200 dark:border-dark-border border-t-1 w-full h-1 col-span-1 hidden xl:block xl:col-span-3 mt-4">
                     </span> 
                     <article class="card col-span-9 col-start-4 z-10 backdrop-blur-sm">
                         <header>
@@ -75,7 +75,7 @@
                     </article>
                 </li>
                 <!-- Arte Regal -->
-                <li class="snap-always md:col-span-6 fadeInUp relative grid grid-cols-12 md:-mt-64">
+                <li class="snap-always xl:col-span-6 fadeInUp relative grid grid-cols-12 xl:-mt-64">
                     <article class="card col-span-9 z-10 backdrop-blur-sm">                    
                         <header>
                             <h3 class="text-2xl font-title font-bold leading-6 mb-1">arte regal</h3>
@@ -87,13 +87,13 @@
                             Mantenimiento y actualización de la ecommerce de la empresa hecha con <b>Codeigniter</b>. Desarrollo de otras webs y ecommerce hechas con <b>Wordpress</b>. Desarrollo de <b>APIS</b>, <b>bloques</b>, <b>temas</b> y <b>plugins</b> para <b>Wordpress</b>.
                         </p>
                     </article>
-                    <span class="border-slate-200 dark:border-dark-border border-t-1 w-full h-1 col-span-3 mt-4">
+                    <span class="border-slate-200 dark:border-dark-border border-t-1 w-full h-1 col-span-1 hidden xl:block xl:col-span-3 mt-4">
                     </span>
-                    <span class="w-fit h-fit py-2 px-6 top-0 col-span-3 text-sm text-center flex justify-center items-center border border-slate-200 dark:border-dark-border absolute right-0 -mr-23 backdrop-blur-sm">06/22<br>10/20</span>
+                    <span class="w-fit h-fit py-2 px-6 top-0 col-span-3 text-sm text-center flex justify-center items-center border border-slate-200 dark:border-dark-border absolute right-0 xl:-mr-23 backdrop-blur-sm">06/22<br>10/20</span>
                 </li>
                 <!-- Availand -->
-                <li class="fadeInUp relative grid grid-cols-12 snap-always md:col-span-6 md:-mt-20">
-                    <span class="w-fit h-fit py-2 px-6 top-0 col-span-3 text-sm text-center flex justify-center items-center border border-slate-200 dark:border-dark-border absolute left-0 -ml-23 backdrop-blur-sm">06/20<br>05/19</span>                     <span class="border-slate-200 dark:border-dark-border border-t-1 w-full h-1 col-span-3 mt-4">
+                <li class="fadeInUp relative grid grid-cols-12 snap-always xl:col-span-6 xl:-mt-20">
+                    <span class="w-fit h-fit py-2 px-6 top-0 col-span-3 text-sm text-center flex justify-center items-center border border-slate-200 dark:border-dark-border absolute left-0 xl:-ml-23 backdrop-blur-sm">06/20<br>05/19</span>                     <span class="border-slate-200 dark:border-dark-border border-t-1 w-full h-1 col-span-1 hidden xl:block xl:col-span-3 mt-4">
                     </span> 
                     <article class="card col-span-9 col-start-4 z-10 backdrop-blur-sm">
                     <header>
@@ -109,7 +109,7 @@
                     </article>
                 </li>
                 <!-- Tv Begin -->
-                <li class="snap-always md:col-span-6 fadeInUp relative grid grid-cols-12 md:-mt-52">  
+                <li class="snap-always xl:col-span-6 fadeInUp relative grid grid-cols-12 xl:-mt-52">  
                     <article class="card col-span-9 z-10 backdrop-blur-sm">                    
                         <header>
                             <h3 class="text-2xl font-title font-bold leading-6 mb-1">tv begin</h3>
@@ -121,14 +121,14 @@
                             Creación y mantenmiento de webs para distintos clientes hechas con Wordpress. Creación de <b>APIS</b>, <b>bloques</b>, <b>plugins</b>, <b>temas</b> y <b>módulos</b> para resolver las necesidades de las emrpesas.
                         </p>
                     </article>
-                    <span class="border-slate-200 dark:border-dark-border border-t-1 w-full h-1 col-span-3 mt-4">
+                    <span class="border-slate-200 dark:border-dark-border border-t-1 w-full h-1 col-span-1 hidden xl:block xl:col-span-3 mt-4">
                     </span>
-                    <span class="w-fit h-fit py-2 px-6 top-0 col-span-3 text-sm text-center flex justify-center items-center border border-slate-200 dark:border-dark-border absolute right-0 -mr-23 backdrop-blur-sm">05/19<br>09/17</span>
+                    <span class="w-fit h-fit py-2 px-6 top-0 col-span-3 text-sm text-center flex justify-center items-center border border-slate-200 dark:border-dark-border absolute right-0 xl:-mr-23 backdrop-blur-sm">05/19<br>09/17</span>
                 </li>
                 <!-- Galaxa Cosmetics -->
-                <li class="fadeInUp relative grid grid-cols-12 snap-always md:col-span-6 md:-mt-20">
-                    <span class="w-fit h-fit py-2 px-6 top-0 col-span-3 text-sm text-center flex justify-center items-center border border-slate-200 dark:border-dark-border absolute left-0 -ml-23 backdrop-blur-sm">06/17<br>04/16</span>
-                    <span class="border-slate-200 dark:border-dark-border border-t-1 w-full h-1 col-span-3 mt-4"></span> 
+                <li class="fadeInUp relative grid grid-cols-12 snap-always xl:col-span-6 xl:-mt-20">
+                    <span class="w-fit h-fit py-2 px-6 top-0 col-span-3 text-sm text-center flex justify-center items-center border border-slate-200 dark:border-dark-border absolute left-0 xl:-ml-23 backdrop-blur-sm">06/17<br>04/16</span>
+                    <span class="border-slate-200 dark:border-dark-border border-t-1 w-full h-1 col-span-1 hidden xl:block xl:col-span-3 mt-4"></span> 
                     <article class="card col-span-9 col-start-4 z-10 backdrop-blur-sm">
                         <header>
                             <h3 class="text-right text-2xl font-title font-bold leading-6 mb-1">galaxa cosmetics</h3>
@@ -144,7 +144,7 @@
         </div>
 
             <div class="w-full h-full absolute flex justify-center items-center -z-10">
-                <div class="md:col-span-1 border-x border-x-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:var(--color-black)]/5 md:block dark:[--pattern-fg:var(--color-white)]/10 h-10/12 py-12 w-10 my-24 mx-auto absolute">
+                <div class="xl:col-span-1 border-x border-x-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:var(--color-black)]/5 xl:block dark:[--pattern-fg:var(--color-white)]/10 h-10/12 py-12 w-10 my-24 mx-auto absolute">
                     <div class="w-full realtive mt-12 fle flex-col">
                     </div>
                 </div>
