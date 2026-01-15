2756e8b777698b03eaff9d1224caa1de03880cbc
crlstve <ecarles10@gmail.com>
2026-01-15 21:02:39 +0100

    padding responsive

--- views/templates/front-page.php
+++ views/templates/front-page.php
@@ -7,7 +7,7 @@
         Functions::partial('experience'); 
 
     ?>
-    <section class="max-w-6xl mx-auto mb-6 lg:py-12 grid grid-cols-12 gap-6 px-6 lg:px-0 lg:gap-12 lg:grid-flow-dense">
+    <section class="max-w-6xl mx-auto mb-6 lg:py-12 grid grid-cols-12 gap-6 lg:gap-12 lg:grid-flow-dense">
         <?php Functions::partial('projects');  ?>
 
         <?php Functions::partial('stack');?>
