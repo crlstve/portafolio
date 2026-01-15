db4fe839f988ca3068da59c266412a998fe1d9ae
crlstve <ecarles10@gmail.com>
2026-01-15 21:02:13 +0100

    padding responsive

--- views/partials/projects.php
+++ views/partials/projects.php
@@ -1,6 +1,6 @@
     <div id="proyectos" class="col-span-12 md:col-span-6 lg:col-span-12 xl:col-span-6 flex flex-col mb-12 md:mb-0">
             <h2 data-text="experiencia" class="w-fit mx-auto font-title p-6 text-3xl md:text-2xl text-center font-bold leading-4 mb-6">proyectos</h2>
-            <ul class="flex flex-col justify-center gap-4 md:gap-8 snap-y snap-mandatory">
+            <ul class="flex flex-col justify-center gap-4 md:gap-8 snap-y snap-mandatory px-2 xl:px-0">
                 <li class="card after:top-0 before:top-0 snap-always md:mt-6 fadeInUp visible mx-auto">
                     <header>
                         <h3 class="text-2xl font-title font-bold leading-4 mb-1">adopta un comercio</h3>
